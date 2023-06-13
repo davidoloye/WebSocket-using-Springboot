ed4612f3dac0f18e076d7705549851d580ff0562
Oluwatomi <ooloye@ecobank.com>
2023-06-13 02:37:47 +0100

    Feat: Implemented the socket client and server logic

---
+++ SpringWebSocket/src/main/java/org/example/GreetingController.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
+
+@RestController
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greet(HelloMessage message){
+        return new Greeting("Hello," + HtmlUtils.htmlEscape(message.getName()));
+
+    }
+}
